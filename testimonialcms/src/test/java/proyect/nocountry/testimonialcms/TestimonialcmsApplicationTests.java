5f765952c81330aa230120d75b953dbbce5f462e
ignacioruiz <ignaruvi@gmail.com>
2026-03-17 23:31:51 -0300

    Added base for backend.

---
+++ testimonialcms/src/test/java/proyect/nocountry/testimonialcms/TestimonialcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package proyect.nocountry.testimonialcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestimonialcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
