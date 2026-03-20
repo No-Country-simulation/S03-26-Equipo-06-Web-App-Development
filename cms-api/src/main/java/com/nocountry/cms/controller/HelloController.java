568e680517a0ebfd7bc3093bfddea6980bd84c12
ignacioruiz <ignaruvi@gmail.com>
2026-03-20 17:10:19 -0300

    Modificación del POM para utilizar las dependencias acordadas en el proyecto

--- cms-api/src/main/java/com/nocountry/cms/controller/HelloController.java
+++ cms-api/src/main/java/com/nocountry/cms/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.nocountry.cms;
+package com.nocountry.cms.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
