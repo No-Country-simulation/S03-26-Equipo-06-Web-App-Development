a864062b46bda80870448939b0bc0c01d7dd91da
riki <ricardosk8532@gmail.com>
2026-03-24 22:17:26 -0300

    Conexion a la base de datos configurada, y creacion de los endpoints get y post.

--- backend/src/main/java/com/nocountry/cms/service/ITestimonioService.java
+++ backend/src/main/java/com/nocountry/cms/service/ITestimonioService.java
@@ -2,9 +2,11 @@ package com.nocountry.cms.service;
 
 import com.nocountry.cms.model.Testimonio;
 
+import java.util.List;
+
 public interface ITestimonioService {
 
     public void createTestimonio(Testimonio testimonio);
-
+    public List<Testimonio> getTestimonios();
 
 }
--- backend/src/main/java/com/nocountry/cms/service/TestimonioService.java
+++ backend/src/main/java/com/nocountry/cms/service/TestimonioService.java
@@ -1,18 +1,28 @@
 package com.nocountry.cms.service;
 
 import com.nocountry.cms.model.Testimonio;
+import com.nocountry.cms.repository.ITestimonioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class TestimonioService implements ITestimonioService {
 
+    @Autowired
+    ITestimonioRepository iTestimonioRepository;
 
     @Override
     public void createTestimonio(Testimonio nuevoTestimonio) {
 
         nuevoTestimonio.setFecha_creacion(LocalDate.now());
+        iTestimonioRepository.save(nuevoTestimonio);
+    }
 
+    @Override
+    public List<Testimonio> getTestimonios() {
+        return iTestimonioRepository.findAll();
     }
 }
