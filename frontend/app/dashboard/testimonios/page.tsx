87fc427e073f36bee37aa1b590e1e93a3af61891
ChristianIvanLedesma <cristuca1234@gmail.com>
2026-03-26 23:35:05 -0300

    fix:solucionando el bug de carpetas

--- frontend/app/dashboard/testimonios/page.tsx
+++ frontend/app/dashboard/testimonios/page.tsx
@@ -1,18 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
 import { Testimonio } from '@/types/testimonio'
 import { TestimonialSkeleton } from '@/app/components/ui/skeletor-cardTestimonio/skeletor'
-
-const TestimonialCard = dynamic<TestimonialCardProps>(
-  () => import('../../components/testimonios/TestimonialCard').then(mod => mod.TestimonialCard),{ ssr: false });
-
-type TestimonialCardProps = {
-  data: Testimonio[]
-}
-
-
-
+import TestimonialCardDashboard from '@/app/components/testimonios/card-dashboard'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
@@ -38,9 +28,18 @@ export default function Testimonios() {
 
   return (
     <div className='min-h-screen bg-linear-to-tr from-[#f8fafc] via-[#eef2f6] to-[#f8fafc]'>
-      <h1 className='pt-13 pb-18 text-center text-2xl font-bold text-gray-800'>Testimonios</h1>
+      <h1 className='pt-13 pb-18 text-center text-2xl font-bold text-gray-800'>
+        Testimonios
+      </h1>
 
-      {loading ? <TestimonialSkeleton /> : <TestimonialCard data={data} />}
+      {loading ? (
+        <TestimonialSkeleton />
+      ) : (
+        <TestimonialCardDashboard 
+          testimonios={data} 
+          loading={loading} 
+        />
+      )}
     </div>
   )
 }
