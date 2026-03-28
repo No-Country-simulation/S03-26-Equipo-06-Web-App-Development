1e3bab997226545ccafca03b432bd9b9426f85e4
ChristianIvanLedesma <cristuca1234@gmail.com>
2026-03-27 21:14:06 -0300

    feat(header): se crea Header y se aplica a layoutClient

--- frontend/app/LayoutClient.tsx
+++ frontend/app/LayoutClient.tsx
@@ -1,15 +1,27 @@
 'use client'
 import { usePathname } from 'next/navigation'
 import Sidebar from './components/sidebar/sidebar'
-
+import Header from './components/header/header'
 export default function LayoutClient({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
   const hideSidebar = pathname === '/'
 
+  const titulo=()=>{
+    if (pathname === '/') return 'Testimonios'
+    if (pathname === '/dashboard') return 'Dashboard'
+    if (pathname === '/dashboard/testimonios') return 'Editar Testimonios'
+    if (pathname === '/dashboard/crear-testimonio') return 'Crear Testimonio'
+
+      return 'Dashboard'
+  }
+
   return (
-    <div className='flex h-full'>
-      {!hideSidebar && <Sidebar />}
-      <main className='flex-1 overflow-y-auto'>{children}</main>
-    </div>
+    <>
+      <Header titulo={titulo()} pathname={pathname} />
+      <div className='flex h-full'>
+        {!hideSidebar && <Sidebar />}
+        <main className='flex-1 overflow-y-auto'>{children}</main>
+      </div>
+    </>
   )
 }
