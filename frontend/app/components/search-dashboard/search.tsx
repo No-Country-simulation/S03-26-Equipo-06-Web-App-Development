dd74241dcde9a307dc6f392a60b2e715099a1356
ChristianIvanLedesma <cristuca1234@gmail.com>
2026-04-06 08:39:45 -0300

    fix(search): corregir values de estado en select (publicado, pendiente, rechazado)

--- frontend/app/components/search-dashboard/search.tsx
+++ frontend/app/components/search-dashboard/search.tsx
@@ -12,7 +12,7 @@ export default function SearchDashboard({ search, setSearch, categoria, setCateg
   const categorias = Array.from(new Set(data.map(t => t.categoria))).sort()
 
   return (
-    <div className='w-92 ml-11 px-7 sm:w-100 sm:mx-auto md:mx-auto flex flex-col items-center justify-center gap-4 pt-10 md:w-[80%] md:flex-row md:items-center md:gap-4'>
+    <div className='w-full px-6 mx-auto md:mx-auto flex flex-col items-center justify-center gap-4 pt-10 md:w-[80%] md:flex-row md:items-center md:gap-4'>
       {/*búsqueda*/}
       <div className='relative w-full md:flex-1'>
         <input
@@ -31,7 +31,7 @@ export default function SearchDashboard({ search, setSearch, categoria, setCateg
         <select
           value={categoria}
           onChange={e => setCategoria(e.target.value)}
-          className='w-1/2 cursor-pointer rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm transition focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 focus:outline-none sm:w-48 lg:w-56'
+          className='w-1/2 cursor-pointer rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm transition focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 focus:outline-none lg:w-56'
         >
           <option value=''>Todas las categorías</option>
           {categorias.map(cate => (
@@ -45,7 +45,7 @@ export default function SearchDashboard({ search, setSearch, categoria, setCateg
         <select
           value={estado}
           onChange={e => setEstado(e.target.value)}
-          className='w-1/2 cursor-pointer rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm transition focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 focus:outline-none sm:w-36 lg:w-40'
+          className='w-1/2 cursor-pointer rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm transition focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 focus:outline-none lg:w-40'
         >
           <option value=''>Todos</option>
           <option value='publicado'>Publicado</option>
